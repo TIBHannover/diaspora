993f62c908c114b4f9889bace00ef874a62fae1e
Rohit Gupta <rgupta0905@gmail.com>
2023-01-25 20:15:12 +0100

    files added

--- wp2/t2.1/v2.0/docker/linux/jenkins/.jen/workspace/diaspora_rdf_map/rdf.py
+++ wp2/t2.1/v2.0/docker/linux/jenkins/.jen/workspace/diaspora_rdf_map/rdf.py
@@ -38,7 +38,7 @@ config.set('default','main_directory', main )
 
 config.add_section('datasets')
 
-number = str(70)
+number = str(77)
 
 output_folder = '${default:main_directory}/Mapping/output' 
 
