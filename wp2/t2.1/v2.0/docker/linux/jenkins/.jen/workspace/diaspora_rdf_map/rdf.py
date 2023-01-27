24e5476761e151f9bd10cd8a68d0935379409434
Rohit Gupta <rgupta0905@gmail.com>
2023-01-27 19:30:58 +0100

    new  ttl files removed

--- wp2/t2.1/v2.0/docker/linux/jenkins/.jen/workspace/diaspora_rdf_map/rdf.py
+++ wp2/t2.1/v2.0/docker/linux/jenkins/.jen/workspace/diaspora_rdf_map/rdf.py
@@ -38,7 +38,7 @@ config.set('default','main_directory', main )
 
 config.add_section('datasets')
 
-number = str(77)
+number = str(70)
 
 output_folder = '${default:main_directory}/Mapping/output' 
 
