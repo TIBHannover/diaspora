5e0545707daa33f0b934158a030bba4010694f02
Rohit Gupta <rgupta0905@gmail.com>
2023-06-08 08:26:28 +0200

    made changes to the duplicate classes

--- wp2/t2.1/v2_1/docker/linux/jenkins/.jen/workspace/diaspora_rdf_map/rdf.py
+++ wp2/t2.1/v2_1/docker/linux/jenkins/.jen/workspace/diaspora_rdf_map/rdf.py
@@ -38,7 +38,7 @@ config.set('default','main_directory', main )
 
 config.add_section('datasets')
 
-num = 18
+num = 75
 number = str(num + 1)
 
 output_folder = '${default:main_directory}/Mapping/output' 
