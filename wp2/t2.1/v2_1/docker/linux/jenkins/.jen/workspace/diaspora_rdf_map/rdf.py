7b3f695f743557922fbea31efc312cd6fe9d8ead
Rohit Gupta <rgupta0905@gmail.com>
2023-06-29 18:01:47 +0200

    table name fixed

--- wp2/t2.1/v2_1/docker/linux/jenkins/.jen/workspace/diaspora_rdf_map/rdf.py
+++ wp2/t2.1/v2_1/docker/linux/jenkins/.jen/workspace/diaspora_rdf_map/rdf.py
@@ -38,7 +38,7 @@ config.set('default','main_directory', main )
 
 config.add_section('datasets')
 
-num = 1
+num = 75
 number = str(num + 1)
 
 output_folder = '${default:main_directory}/Mapping/output' 
